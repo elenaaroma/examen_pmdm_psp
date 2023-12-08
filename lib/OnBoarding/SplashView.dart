7c7c803b67d55092936cc0a40b925ff598348055
Elena <elenaromadam@gmail.com>
2023-12-08 16:09:49 +0100

    PMDM: creacion de SplashView.dart

--- lib/OnBoarding/SplashView.dart
+++ lib/OnBoarding/SplashView.dart
@@ -1,10 +1,36 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    checkSesion();
   }
 
+  void checkSesion() async{
+    await Future.delayed(Duration(seconds: 3));
+    Navigator.of(context).popAndPushNamed("/loginview");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircularProgressIndicator()
+          ],
+        ),
+      ),
+    );
+  }
 }
