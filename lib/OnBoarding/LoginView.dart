2c6dbb1087d2821d2e374cc1c72d987565b41274
Elena <elenaromadam@gmail.com>
2023-12-09 16:50:47 +0100

    PMDM: boton aceptar de LoginView va a home validando el usuario

--- lib/OnBoarding/LoginView.dart
+++ lib/OnBoarding/LoginView.dart
@@ -1,6 +1,10 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:examen_pmdm/CustomViews/ButtonCustom.dart';
 import 'package:examen_pmdm/CustomViews/TextCustom.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+import '../FirestoreObjects/FbUsuario.dart';
 
 
 class LogingView extends StatelessWidget{
@@ -10,9 +14,36 @@ class LogingView extends StatelessWidget{
   TextEditingController usernameController=TextEditingController();
   TextEditingController passwordController=TextEditingController();
 
-  void OnClickAceptar() {
-  Navigator.of(_context).popAndPushNamed("/homeview");
-}
+  FirebaseFirestore db = FirebaseFirestore.instance;
+
+  throwSnackBar(String error){
+    SnackBar snackBar = SnackBar(
+      content: Text(error),
+    );
+    ScaffoldMessenger.of(_context).showSnackBar(snackBar);
+  }
+
+  Future<void> onClickAceptar() async {
+    try {
+      await FirebaseAuth.instance.signInWithEmailAndPassword(
+          email: usernameController.text,
+          password: passwordController.text
+      );
+      Navigator.of(_context).popAndPushNamed('/homeview');
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'invalid-email') {
+        print(' --> Formato del email incorrecto.');
+        throwSnackBar('-- Formato del email incorrecto --');
+      } else if (e.code == 'invalid-login-credentials') {
+        print(' --> Credenciales incorrectas.');
+        throwSnackBar('-- Credenciales incorrectas --');
+      }
+    }
+  }
+
+  void OnClickRegistrar() {
+    Navigator.of(_context).popAndPushNamed("/registergview");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -33,7 +64,9 @@ class LogingView extends StatelessWidget{
                 TextCustom(tecController: usernameController, icon: Icon(Icons.account_circle) , sHint: 'Escribe el usuario', colorCaja: Colors.green[200] ?? Colors.green, colorTexto: Colors.white, colorBorde: Colors.green[400] ?? Colors.green,),
                 TextCustom(tecController: passwordController, icon: Icon(Icons.lock_outline) , sHint: 'Escribe la contraseña', colorCaja: Colors.green[200] ?? Colors.green, colorTexto: Colors.white, colorBorde: Colors.green[400] ?? Colors.green, blIsPassword: true,),
 
-                ButtonCustom(sText: "Aceptar", event: OnClickAceptar, fondo: Colors.green[200] ?? Colors.green, letra: Colors.white, alto: 20, ancho: 30)
+                ButtonCustom(sText: "Aceptar", event: onClickAceptar, fondo: Colors.green[200] ?? Colors.green, letra: Colors.white, alto: 20, ancho: 30),
+                ButtonCustom(sText: "Registrar", event: OnClickRegistrar, fondo: Colors.green[200] ?? Colors.green, letra: Colors.white, alto: 20, ancho: 30)
+
               ],
             )
           ,
